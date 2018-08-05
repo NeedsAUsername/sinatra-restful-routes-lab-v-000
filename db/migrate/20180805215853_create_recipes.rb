cf61abe42907451826133d64ce7aaefdbe9e6e2b
Kevin Chi <kevinchi118@gmail.com>
2018-08-05 21:59:18 +0000

    automatically backed up by learn

--- db/migrate/20180805215853_create_recipes.rb
+++ db/migrate/20180805215853_create_recipes.rb
@@ -1,4 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_tables :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
   end
 end
